3b092ef1b4abaf3ad82c45e419036018c1632468
ManasRaipure <manasraipure2002@gmail.com>
2024-05-16 10:37:28 +0000

    Strings

---
+++ 01_basic/5.Strings.js
@@ -0,0 +1,22 @@
+// const name = "manas" 
+// const age = 21
+// console.log(`my name is ${name} and my age is ${age}`);
+
+// const gamename= new String ("killwithskill")
+// // console.log(gamename.length);
+// console.log(gamename.indexOf('i')); // return the index no of specific alphabet
+// console.log(gamename.charAt(1)); // return the alphabet at the sepecific index no
+// console.log(gamename.toUpperCase()) // convert the string into uppercase letters
+
+// const newstring = gamename.slice( 4,9)
+// console.log(newstring);
+//  const anotherstring = gamename.substring(0,9)
+//  console.log(anotherstring); // it gives the alphabet at the respective index no.
+
+//  const lateststring = new String("    manas    ")
+//  console.log(lateststring.trim()) // it removes extra spaces from the string
+
+
+ const url = "https://www.google.com//home%20google"
+ console.log(url.replace('%20', '-'))
+ console.log(url.includes('home'))// it checks wheather the string include the following word or not

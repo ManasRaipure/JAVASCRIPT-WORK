2b7a6111acc41e94b60f95c55e04add7915e5b91
ManasRaipure <manasraipure2002@gmail.com>
2024-05-17 11:59:10 +0000

    Array

---
+++ 02_basic/1.Array.js
@@ -0,0 +1,41 @@
+const myArr = [0, 1 , 2 , 3, 4, 5]
+const  marvel_heros = ["spiderman" , 'thor', 'captain america']
+
+// console.log( myarr)
+
+// console.log(marvel_heros)
+
+
+
+// Array Methods
+
+// myarr.push(6)
+// myarr.push(8)
+// myarr.pop()
+// myarr.unshift(1)
+// myarr.shift()
+
+
+
+// console.log( myarr.includes(9))
+// console.log( myarr.indexOf(3))
+
+// const newarr = myarr.join()// converts the datatype of array into string
+// console.log(myarr)
+// console.log(newarr)
+
+
+// Slice , Splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+console.log(myn2);
+

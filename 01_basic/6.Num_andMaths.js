f7f04c5c0ae352da5e5ff895c3d38fbcef024d70
ManasRaipure <manasraipure2002@gmail.com>
2024-05-17 07:28:39 +0000

    Numbers and Maths

---
+++ 01_basic/6.Num_andMaths.js
@@ -0,0 +1,32 @@
+// const score = 33
+// console.log(score);
+
+// const balance = 100
+// console.log(balance.toFixed(2)); // adds two zeros in decimal
+// console.log(balance.toString().length);
+
+// const Anothernumber = 123.899
+// console.log(Anothernumber.toPrecision(4)) // it gives the exact value
+
+// const hundred = 1000000
+// console.log(hundred.toLocaleString('en-IN')); // represent the the no. in indian format
+
+
+// //*************************Maths**********************************//
+
+// console.log(Math)
+// console.log(Math.abs(-4)) // converts the negative value into positive
+// console.log(Math.round(4.4))// take the round figure value not in decimal
+// console.log(Math.ceil(4.6)) // consider the upper value 
+// console.log(Math.floor(4.9))// consider the lower value
+// console.log(Math.min(3,4,5,6,))
+// console.log(Math.max(3,4,5,6,))
+
+// console.log(Math.random()) // return the value only in 0 and 1
+// console.log(Math.random()*10)+1
+
+const min = 10
+const max= 20
+
+console.log(Math.round(Math.random()* (max - min + 1)) + min )
+

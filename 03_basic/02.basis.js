51a0db21be70a51fc662dd2923c1447f7017051c
ManasRaipure <manasraipure2002@gmail.com>
2024-05-22 07:45:23 +0000

    function scope

---
+++ 03_basic/02.basis.js
@@ -0,0 +1,50 @@
+
+
+let d = 4 
+
+if(true){
+let b = 2
+const c = 3
+
+// console.log(d)
+}
+
+
+
+// console.log(b)
+// console.log(c)
+
+function one (){
+    const username = "Manas"
+    function two (){
+        const webpage = "Youtube"
+
+        // console.log (username)
+    }
+    // console.log(webpage)
+    two()
+}
+one()
+
+
+if(true){
+    const username ="Manas"
+    if(username==="Manas"){
+        const web = "Youtube"
+        // console.log(username + web)
+
+
+    }
+   
+}
+
+function add (num){
+    return num +1
+}
+console.log(add(1))
+
+const addfun= function(num){
+    return num+1
+}
+console.log(addfun(2))
+

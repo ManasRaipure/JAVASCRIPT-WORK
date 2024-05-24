d3ae288feceb398fe48af5dcc663bd90a412ba6b
ManasRaipure <manasraipure2002@gmail.com>
2024-05-24 07:36:37 +0000

    switch

---
+++ 04_control_flow/02.switch.js
@@ -0,0 +1,38 @@
+const month = 3
+
+switch (month) {
+    case 1: console.log("january")
+        
+        break;
+    case 2: console.log("feb")
+        
+        break;
+    case 3: console.log("march")
+        
+        break;
+    case 4: console.log("april")  
+        break;
+
+    default:
+        break;
+}
+
+
+const month1 = "january"
+
+switch (month1) {
+    case "january": console.log("january")
+        
+        break;
+    case "feb": console.log("feb")
+        
+        break;
+    case "march": console.log("march")
+        
+        break;
+    case "april": console.log("april")  
+        break;
+
+    default:
+        break;
+}

be3c1ab113281ff887c508975537be70bea91d9a
ManasRaipure <manasraipure2002@gmail.com>
2024-05-25 07:33:42 +0000

    for in loop

---
+++ 05_iterations/04.for-in.js
@@ -0,0 +1,29 @@
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
+// for in loop
+
+const myobject = {
+    js:"javascript",
+    py:"python",
+    rb:"ruby"
+}
+
+for (const key in myobject ) {
+
+    // console.log(key , myobject[key] )
+   
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    console.log(programming[key]);
+}

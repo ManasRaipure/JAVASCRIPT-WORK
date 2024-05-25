cd599b850a9cb9b63a48eb44f9d109561b7b88bb
ManasRaipure <manasraipure2002@gmail.com>
2024-05-25 06:12:10 +0000

    while and do-while loop

---
+++ 05_iterations/02.while&do-while.js
@@ -0,0 +1,26 @@
+
+
+let index = 0
+while (index<=10) {
+
+    // console.log(`value is ${index}`)
+    index= index+2
+}
+
+let myarray = ["batman",'superman', 'spiderman']
+let arr = 0
+
+while (arr<myarray.length) {
+    // console.log(myarray[arr]);
+
+    arr++
+    
+}
+
+
+let score = 11
+do {
+    console.log(score);
+    score ++
+    
+} while (score<=10); // in this loop value will always print first then it will check the condition.
